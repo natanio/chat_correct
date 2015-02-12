fd857a75b611728c22e89e1bbb5f139fd2dc7c1f
Nate Hill <natanio@gmail.com>
2015-02-12 12:03:04 +0900

    capitalization test for first word in string

--- lib/chat_correct/capitalization.rb
+++ lib/chat_correct/capitalization.rb
@@ -7,7 +7,7 @@ module ChatCorrect
     end
 
     def capitalization_error?
-      word_a.downcase.eql?(word_b.downcase) && (word_a != word_b)
+      word_a.downcase.eql?(word_b.downcase) && (word_a != word_b) && (cc.values[0] == word_b)
     end
   end
 end
--- spec/chat_correct/capitalization_spec.rb
+++ spec/chat_correct/capitalization_spec.rb
@@ -28,4 +28,11 @@ RSpec.describe ChatCorrect::Capitalization do
     cc = ChatCorrect::Capitalization.new(token_a: token_a, token_b: token_b)
     expect(cc.capitalization_error?).to eq(true)
   end
+
+  it 'should return false if missing text is added before the token' do
+    token_a = 'They'
+    token_b = 'they'
+    cc = ChatCorrect::Capitalization.new(token_a: token_a, token_b: token_b)
+    expect(cc.capitalization_error?).to eq(true)
+  end
 end
